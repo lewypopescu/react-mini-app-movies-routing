c7abab033a6804a47ee0deeeaecd33a55d75743c
lewypopescu <popesculewy@gmail.com>
2024-08-05 16:07:34 +0200

    good home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,9 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />;
-        <Route path="/movies/:movieId/cast" element={<Cast />} />;
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />;
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
       </Routes>
     </>
   );
